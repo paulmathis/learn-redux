e3e0627b8152ea57a22f09313f9b71b58a517652
Paul Mathis <pmathis@abor.com>
2017-11-22 13:44:06 -0600

    Vid-3 1:37

---
+++ client/components/Single.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class Single extends Component {
+  render() {
+    return <div className="single-photo">I'm the sinle</div>;
+  }
+}
+
+export default Single;
