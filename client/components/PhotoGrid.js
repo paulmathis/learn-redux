2e318a89324382bd0e003ac5e780ca06fd2ec112
Paul Mathis <pmathis@abor.com>
2017-12-13 16:58:56 -0600

    Vid 12 Start

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import Photo from './Photos';
 
 class PhotoGrid extends Component {
   render() {
-    return <div className="photo-grid">I'm the photo-grid</div>;
+    return (
+      <div className="photo-grid">
+        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
+      </div>
+    );
   }
 }
 
